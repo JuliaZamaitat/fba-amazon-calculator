fe3ce415afe05ca528164e0572baf19e871889f7
JuliaZamaitat <j.zamaitat@gmail.com>
2021-10-18 19:04:25 +0200

    adding netlify.toml

---
+++ dist/_nuxt/static/1634576655/blog/a-themeable-react-data-grid-with-great-ux-focused-features/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634576655",layout:"default",error:a,state:{posts:[]},serverRendered:true,routePath:"\u002Fblog\u002Fa-themeable-react-data-grid-with-great-ux-focused-features",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
