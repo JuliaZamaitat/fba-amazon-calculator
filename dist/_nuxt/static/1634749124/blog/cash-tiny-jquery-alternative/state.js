ba87a51e781a92cfe043ed8e29ccb62b593f87aa
JuliaZamaitat <j.zamaitat@gmail.com>
2021-10-20 18:59:06 +0200

    typo fix

---
+++ dist/_nuxt/static/1634749124/blog/cash-tiny-jquery-alternative/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634749124",layout:"default",error:a,state:{posts:[]},serverRendered:true,routePath:"\u002Fblog\u002Fcash-tiny-jquery-alternative",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
