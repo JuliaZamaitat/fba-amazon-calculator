dc4e3c4ea6c636dd3acd6a9065bdab7227ef0411
JuliaZamaitat <j.zamaitat@gmail.com>
2021-10-20 22:12:35 +0200

    config wip

---
+++ dist/_nuxt/static/1634760746/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fblog\u002Fwill-it-break","\u002Fblog\u002Ftest-eintrag","\u002Fblog\u002Fnoch-ein-eintrag","\u002Fblog\u002Fhello-world","\u002Fblog"]})
