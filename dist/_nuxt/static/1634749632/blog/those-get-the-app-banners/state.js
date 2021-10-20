4f8e04f2d3d3fb0210e1ef79167ab1c72430e31c
JuliaZamaitat <j.zamaitat@gmail.com>
2021-10-20 19:13:59 +0200

    wip config

--- dist/_nuxt/static/1634749632/blog/those-get-the-app-banners/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634749632",layout:"default",error:a,state:{posts:[]},serverRendered:true,routePath:"\u002Fblog\u002Fthose-get-the-app-banners",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
