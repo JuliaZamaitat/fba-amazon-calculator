d5f83a383f8bb94b338c5ee6e98bf4ca1619e259
JuliaZamaitat <j.zamaitat@gmail.com>
2021-11-08 18:45:49 +0100

    first quiz page finished

---
+++ components/Disclaimer.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="disclaimer">
+    <p>
+      * Bei dem errechneten Unternehmenswert handelt es sich lediglich um eine
+      ungefähre Ersteinschätzung. Für eine exakte Ermittlung sind weitere Daten
+      und Prüfungen notwendig. Der errechnete Unternehmenswert stellt kein
+      verbindliches Kaufangebot dar.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.disclaimer {
+  p {
+    font-family: 'Montserrat', sans-serif;
+    font-style: italic;
+    font-weight: var(--fw-thin);
+    font-size: 12px;
+    color: var(--clr-white-100);
+    opacity: 80%;
+    width: 80vw;
+    position: absolute;
+    bottom: 2em;
+    margin-left: 6.6em;
+    line-height: 15px;
+    letter-spacing: 0.1em;
+  }
+}
+</style>
--- pages/quiz.vue
+++ pages/quiz.vue
@@ -2,37 +2,68 @@
   <div>
     <img class="bg-bottom" src="../assets/backgrounds/bg-bottom-purple.svg" />
     <img class="icon-quiz-1" src="../assets/icons/icon-quiz-1.svg" />
-
     <div class="questions">
       <div class="section-1">
         <h2>Seit wann verkaufst du aktiv auf Amazon?</h2>
-        <client-only
-          ><p>
+        <div class="calendar">
+          <img class="icon-calendar" src="../assets/icons/calendar.svg" />
+          <client-only>
             <date-picker
               :value="date"
               name="uniquename"
-              inputClass="myClass"
-            /></p
-        ></client-only>
+              inputClass="datepicker"
+          /></client-only>
+        </div>
+        <img class="icon-line" src="../assets/icons/line.svg" />
       </div>
+
       <div class="section-2">
         <h2>Anteil von Amazon Umsatz in Prozent von Gesamtumsatz</h2>
         <p>
           Umsatz deiner über Amazon erzielten Verkäufe im Verhältnis zum
           Gesamtumsatz.
         </p>
-        <p>PLATZHALTER PLATZHALTER</p>
+        <div>
+          <p>%</p>
+          <input
+            type="text"
+            placeholder="xx"
+            v-model="percentage"
+            @input="checkPercentage()"
+          />
+        </div>
+        <img class="icon-line" src="../assets/icons/line.svg" />
       </div>
-      <button @click="next()">Weiter</button>
+
+      <div class="validation">
+        <ul>
+          <li class="error-message" v-for="error in errors" :key="error.id">
+            {{ error }}
+          </li>
+        </ul>
+      </div>
+      <button
+        @click="next()"
+        :disabled="errors.length != 0 || percentage == ''"
+        :class="{ disabled: errors.length != 0 || percentage == '' }"
+      >
+        Weiter
+      </button>
     </div>
+
+    <Disclaimer />
   </div>
 </template>
 
 <script>
+import Disclaimer from '~/components/Disclaimer.vue';
 export default {
+  components: { Disclaimer },
   data() {
     return {
-      date: new Date()
+      date: new Date(),
+      errors: [],
+      percentage: ''
     };
   },
   mounted() {
@@ -42,15 +73,52 @@ export default {
       'var(--clr-purple-100)';
   },
   methods: {
-    next() {}
+    next() {},
+    checkPercentage() {
+      this.errors = [];
+      if (
+        isNaN(this.percentage) ||
+        this.percentage > 100 ||
+        this.percentage < 0
+      ) {
+        this.errors.push('Ungültige Prozentangabe');
+      }
+    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
+// .page-enter-active,
+// .page-leave-active {
+//   position: absolute;
+//   max-width: 100%; /*make sur our content keep it's original width*/
+//   transition: all 1s ease;
+// }
+
+// /* entering start */
+// .page-enter {
+//   top: -100%;
+// }
+
+// /* entering end */
+// .page-enter-to {
+//   top: 0;
+// }
+
+// /* leaving start */
+// .page-leave {
+//   opacity: 1;
+// }
+
+// /* leaving end */
+// .page-leave-to {
+//   opacity: 0;
+// }
+
 .questions {
   text-align: right;
-  margin-top: 4em;
+  margin-top: 2em;
   margin-left: 4em;
   max-width: 40em;
 
@@ -74,25 +142,100 @@ export default {
     margin-right: 4em;
     font-weight: var(--fw-bold);
     &:hover {
-      background-color: white;
+      background-color: var(--clr-white-100);
       color: var(--clr-orange-100);
+    }
+    &.disabled {
+      cursor: auto;
+      border: 2px solid rgb(144, 144, 144);
+      color: rgb(144, 144, 144);
+
+      &:hover {
+        background-color: transparent;
+        color: rgb(144, 144, 144);
+      }
     }
   }
 
+  .icon-line {
+    margin-top: 0.5em;
+  }
   .section-1 {
-    p {
-      margin-top: 2.4em;
+    .calendar {
+      font-family: 'Montserrat', sans-serif;
+      text-align: left;
+      font-weight: var(--fw-thin);
+      display: flex;
+      align-items: self-end;
+
+      .icon-calendar {
+        display: inline;
+        margin-right: 38px;
+      }
+
+      ::v-deep .vdp-datepicker {
+        display: inline;
+
+        div:first-of-type {
+          display: inline;
+        }
+      }
+
+      ::v-deep .datepicker {
+        margin-top: 2.4em;
+        border: none;
+        background: transparent;
+        color: var(--clr-white-100);
+        letter-spacing: 0.1em;
+
+        &:hover {
+          cursor: pointer;
+          font-weight: var(--fw-regular);
+        }
+
+        &:focus-visible {
+          outline: none;
+        }
+      }
+
+      ::v-deep .vdp-datepicker__calendar {
+        border: none;
+        border-radius: 10px;
+      }
     }
   }
   .section-2 {
-    margin-top: 2em; //4.15em;
+    margin-top: 3em; //4.15em;
+    text-align: left;
 
     p:first-of-type {
       margin-top: 0.6em;
+      letter-spacing: 0.1em;
     }
 
-    p:last-of-type {
-      margin-top: 1.9em;
+    div {
+      padding-top: 1.9em;
+      p:first-of-type {
+        font-weight: var(--fw-bold);
+        display: inline;
+      }
+      input {
+        cursor: pointer;
+        background: none;
+        border: none;
+        color: var(--clr-white-100);
+        font-weight: var(--fw-thin);
+        letter-spacing: 0.1em;
+
+        &::placeholder {
+          color: var(--clr-white-100);
+          font-weight: var(--fw-thin);
+        }
+
+        &:focus-visible {
+          outline: none;
+        }
+      }
     }
   }
 }
@@ -103,5 +246,18 @@ export default {
   right: 9em;
   max-height: 100vh;
   z-index: -10;
+}
+
+.validation {
+  display: inline;
+  ul,
+  li {
+    display: inline;
+  }
+
+  .error-message {
+    color: rgb(252, 0, 0);
+    margin-right: 0.3em;
+  }
 }
 </style>
