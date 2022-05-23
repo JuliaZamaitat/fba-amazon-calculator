2796ed59aaf3cb7b7d6cb98052c01a352907e4ed
JuliaZamaitat <j.zamaitat@gmail.com>
2022-05-23 14:23:58 +0200

    adding https for requesting data from wordpress.

--- helper/url.js
+++ helper/url.js
@@ -2,7 +2,7 @@ var baseURL;
 // if (process.env.NODE_ENV !== 'development') {
 //   baseURL = '/api';
 // } else {
-baseURL = `http://b10ptpl.myraidbox.de/wp-json/wp/v2`;
+baseURL = `https://b10ptpl.myraidbox.de/wp-json/wp/v2`;
 // }
 
 module.exports = {
